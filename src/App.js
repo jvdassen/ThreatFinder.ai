8a4d1de00fb381dfbc53b8a8b7d948e610726df1
JSha91 <jamo.sharif@uzh.ch>
2023-11-12 22:45:13 +0100

    Corrections --> (1) const handleFileChange (2) handleUpload (3) getTestDivs

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from "react";
-import {api} from './utils/api'
+import { useEffect, useState } from "react";
+import { api } from './utils/api'
+import axios from "axios";
 
 function App() {
-  const [data, setData] = useState("");
+  const [data, setData] = useState([]);
+  const [file, setFile] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await api.get('/');
-        setData(response.data.message);
-      } catch (error) {
-        alert(error.response.data.message);
-      }
-    };
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
 
-    fetchData();
-  }, []);
+  const handleUpload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const response = await axios.post('http://localhost:8080/upload', formData);
+      setData(response.data);
+    } catch (error) {
+      alert(error.response.data);
+    }
+  };
+
+  const getTestDivs = data => {
+    const result = data.map(asset => asset['Threats'].map(threat => (
+        <>
+          <div>Asset name: {threat['Affected assets']}</div>
+          <div>"Threat Category":{threat["Threat Category"]},</div>
+            <div>"Threat": {threat["Threat"]},</div>
+              <div> "Description": {threat["De}scription"]},</div>
+                <div>"Potential Impact":{threat["Potential Impact"]},</div>
+                  <div>"Affected assets": {threat['Affected assets']}</div>
+        </>
+    )))
+    return <>{result}</>;
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>{data}</p>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            ThreatsFinder for AI-based Systems
+          </p>
+          <input type="file" onChange={handleFileChange} />
+          <button onClick={handleUpload}>Upload and Identify Threats</button>
+          {data.length > 0 && getTestDivs(data)}
+        </header>
+      </div>
   );
 }
 
