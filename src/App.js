32f3cbdab197ab55e99731e2b07a741f3dff1d77
JSha91 <jamo.sharif@uzh.ch>
2023-11-16 14:25:15 +0100

    Restructuring (1) Improved design and Code

--- src/App.js
+++ src/App.js
@@ -1,54 +1,59 @@
 import './App.css';
-import { useEffect, useState } from "react";
-import { api } from './utils/api'
+import {useState} from "react";
 import axios from "axios";
 
 function App() {
-  const [data, setData] = useState([]);
-  const [file, setFile] = useState(null);
+    const [data, setData] = useState([]);
+    const [file, setFile] = useState(null);
 
-  const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
-  };
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+    };
 
-  const handleUpload = async () => {
-    try {
-      const formData = new FormData();
-      formData.append("file", file);
+    const handleUpload = async () => {
+        try {
+            const formData = new FormData();
+            formData.append("file", file);
 
-      const response = await axios.post('http://localhost:8080/upload', formData);
-      setData(response.data);
-    } catch (error) {
-      alert(error.response.data);
+            const response = await axios.post('http://localhost:8080/upload', formData);
+            setData(response.data);
+        } catch (error) {
+            alert(error.response.data);
+        }
+    };
+
+    const getTestDivs = data => {
+        return (<div style={{textAlign: "left"}}>
+            {data.map((asset) => (<ul>
+                <li>Identified Asset : {asset["Identified Asset"]}</li>
+                <li>Category: {asset["Category"]}</li>
+                <li>All possible Threats:</li>
+                <ol type="1">
+                {asset["Threats"].map((threat) => (<>
+                    <li></li>
+                    <ul>
+                        <li>Threat: {threat["Threat"]}</li>
+                        <li>Threat Category: {threat["Threat Category"]}</li>
+                        <li>Potential Impact: {threat["Potential Impact"]}</li>
+                        <li>Description: {threat["Description"]}</li>
+                    </ul>
+                </>))}
+                </ol>
+            </ul>))}
+        </div>);
     }
-  };
 
-  const getTestDivs = data => {
-    const result = data.map(asset => asset['Threats'].map(threat => (
-        <>
-          <div>Asset name: {threat['Affected assets']}</div>
-          <div>"Threat Category":{threat["Threat Category"]},</div>
-            <div>"Threat": {threat["Threat"]},</div>
-              <div> "Description": {threat["De}scription"]},</div>
-                <div>"Potential Impact":{threat["Potential Impact"]},</div>
-                  <div>"Affected assets": {threat['Affected assets']}</div>
-        </>
-    )))
-    return <>{result}</>;
-  }
-
-  return (
-      <div className="App">
+    return (<div className="App">
         <header className="App-header">
-          <p>
-            ThreatsFinder for AI-based Systems
-          </p>
-          <input type="file" onChange={handleFileChange} />
-          <button onClick={handleUpload}>Upload and Identify Threats</button>
-          {data.length > 0 && getTestDivs(data)}
+            <h1>
+                ThreatsFinder for AI-based Systems
+            </h1>
+            <input type="file" onChange={handleFileChange}/>
+            <button onClick={handleUpload}>Upload and Identify Threats</button>
+            <br/>
+            {data && getTestDivs(data)}
         </header>
-      </div>
-  );
+    </div>);
 }
 
 export default App;
